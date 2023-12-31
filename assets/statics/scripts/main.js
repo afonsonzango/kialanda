3fa3e5d9c9f80019f1b916dd8befaf00792a04c3
Afonso Nzango <afonso1.nzango@gmail.com>
2023-12-31 16:21:10 +0100

    first commit

---
+++ assets/statics/scripts/main.js
@@ -0,0 +1,45 @@
+var mobilebutton = document.getElementById('mobile-button');
+var lnkcontainer = document.getElementById('lnk-container');
+
+lnkcontainer.style.left = '-100%'
+
+mobilebutton.addEventListener('click', function () {
+    if (lnkcontainer.style.left == '0px') {
+        lnkcontainer.style.left = '-100%'
+    } else {
+        lnkcontainer.style.left = '0px'
+    }
+});
+
+document.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A') {
+        event.preventDefault();
+        lnkcontainer.style.left = '-100%';
+    }
+});
+
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (localStorage.getItem('kialanda-theme-color') == 'dark') {
+    document.querySelector("html").setAttribute("data-theme", 'dark');
+} else if (localStorage.getItem('kialanda-theme-color') == 'light') {
+    document.querySelector("html").setAttribute("data-theme", 'light');
+} else if (systemSettingDark.matches) {
+    document.querySelector("html").setAttribute("data-theme", 'dark');
+}
+
+
+
+const button = document.querySelector("[data-theme-toggle]");
+
+button.addEventListener('click', function () {
+    var actualtheme = document.querySelector("html").getAttribute("data-theme");
+
+    if (actualtheme == 'dark') {
+        localStorage.setItem('kialanda-theme-color', 'light');
+        document.querySelector("html").setAttribute("data-theme", 'light');
+    } else {
+        localStorage.setItem('kialanda-theme-color', 'dark');
+        document.querySelector("html").setAttribute("data-theme", 'dark');
+    }
+})
---
+++ router/script.js
@@ -0,0 +1,56 @@
+function loadPage(page) {
+    addProgress();
+    const contentDiv = document.getElementById('content');
+
+    fetch(`${page}.html`)
+        .then(response => response.text())
+        .then(data => {
+            contentDiv.innerHTML = data;
+            removeProgress();
+        })
+        .catch(error => {
+            console.error(`Error loading ${page}.html:`, error);
+            contentDiv.innerHTML = `<p>Error loading ${page}.html: ${error.message}</p>`;
+            removeProgress();
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Initial data load for the Home page
+    loadPage('/pages/introducao');
+
+    // Event listener for navigation links
+    document.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A') {
+            event.preventDefault();
+            const page = event.target.getAttribute('href').substring(1);
+            loadPage(page);
+        }
+    });
+});
+
+function addProgress() {
+    if (!document.querySelector('#progress')) {
+        const progressDiv = document.createElement('div');
+        progressDiv.id = 'progress';
+        document.body.appendChild(progressDiv);
+
+        setTimeout(() => {
+            progressDiv.style.width = '70%';
+        }, 100);
+    }
+}
+
+function removeProgress() {
+    setTimeout(() => {
+        const progressDiv = document.querySelector('#progress');
+        if (progressDiv) {
+            progressDiv.style.transition = '.2s';
+            progressDiv.style.width = '100%';
+
+            setTimeout(() => {
+                progressDiv.remove();
+            }, 600);
+        }
+    }, 100);
+}
