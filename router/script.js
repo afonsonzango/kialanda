0f381497a58660eac1908ef565376b1c9a1387a7
Afonso Nzango <afonso1.nzango@gmail.com>
2024-01-03 00:01:41 +0100

    Setted up styles and routing

--- router/script.js
+++ router/script.js
@@ -1,18 +1,43 @@
-function loadPage(page) {
+function loadCSS(filePath) {
+    // Remove the existing style element
+    var existingStyle = document.getElementById("page-styles");
+
+    if (existingStyle) {
+        existingStyle.parentNode.removeChild(existingStyle);
+    }
+
+    // Create a new style element
+    var styleElement = document.createElement("style");
+    styleElement.id = "page-styles";
+
+    // Set the content of the style element to the contents of the CSS file
+    fetch(filePath)
+    .then(response => response.text())
+    .then(cssContent => {
+        styleElement.textContent = cssContent;
+    })
+    .catch(error => console.error("Error loading CSS file:", error));
+
+    // Append the style element to the head of the document
+    document.head.appendChild(styleElement);
+}
+
+function loadPage(page, css) {
     addProgress();
     const contentDiv = document.getElementById('content');
 
     fetch(`${page}.html`)
-        .then(response => response.text())
-        .then(data => {
-            contentDiv.innerHTML = data;
-            removeProgress();
-        })
-        .catch(error => {
-            console.error(`Error loading ${page}.html:`, error);
-            contentDiv.innerHTML = `<p>Error loading ${page}.html: ${error.message}</p>`;
-            removeProgress();
-        });
+    .then(response => response.text())
+    .then(data => {
+        contentDiv.innerHTML = data;
+        loadCSS(css);
+        removeProgress();
+    })
+    .catch(error => {
+        console.error(`Error loading ${page}.html:`, error);
+        contentDiv.innerHTML = `<p>Error loading ${page}.html: ${error.message}</p>`;
+        removeProgress();
+    });
 }
 
 function addProgress() {
@@ -47,7 +72,8 @@ window.addEventListener('load', function () {
         if (event.target.tagName === 'A') {
             event.preventDefault();
             const page = event.target.getAttribute('href').substring(1);
-            loadPage(page);
+            const css = event.target.getAttribute('data-css');
+            loadPage(page, css);
         }
     });
 });
